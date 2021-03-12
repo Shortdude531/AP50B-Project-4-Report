67a69662a42b161bc10cbd71e0be1b11b47887d5
Shortdude531 <55025736+Shortdude531@users.noreply.github.com>
2021-03-11 20:16:43 -0500

    Update Moisture_Sensor_Final_Setup_Code.ino

--- Moisture_Sensor_Final_Setup_Code.ino
+++ Moisture_Sensor_Final_Setup_Code.ino
@@ -16,7 +16,6 @@ int soilPin = A2;//Declare a variable for the soil moisture sensor
 int soilPower = A3;//Variable for Soil moisture Power
 seesaw_Motor motor_a(&crickit); //inialize dc motor'
 
-
 //Rather than powering the sensor through the 3.3V or 5V pins, 
 //we'll use a digital pin to power the sensor. This will 
 //prevent corrosion of the sensor as it sits in the soil. 
@@ -30,7 +29,6 @@ void setup()
 
 void loop() 
 {
-  
   //delay
   delay(1000);//take a reading every second to prevent Corrosion
   Serial.begin(9600);
@@ -61,8 +59,6 @@ void loop()
       //turns on growing lights
       crickit.pinMode(LED_1, OUTPUT);
       crickit.digitalWrite(LED_1, HIGH);
-
-  
     }
 
     //if the soild is drying out
@@ -90,28 +86,19 @@ void loop()
         CircuitPlayground.strip.show();
 
         //plays alarm when water level is critical
-        CircuitPlayground.playTone(500, 100);
-        
+        CircuitPlayground.playTone(500, 100); 
         //initiates crickit
         crickit.begin();
         //turns growing lights off
         crickit.pinMode(LED_1, OUTPUT);
-        crickit.digitalWrite(LED_1, LOW);
-
-        
+        crickit.digitalWrite(LED_1, LOW);  
         //start watering cycle using servo motor
         motor_a.attach(CRICKIT_MOTOR_A1, CRICKIT_MOTOR_A2);
         motor_a.throttle(-.5);
         delay(500);
         motor_a.throttle(0);
-    }   
+      }   
     }
-
- 
- 
-  
-  
-
 //This is a function used to get the soil moisture content from sparks fun code
 int readSoil()
 {
